73e1fb0801f7568db3f6fa29ace99f1df70e1a65
Lucía <luciasantillanadiaz@gmail.com>
2024-05-21 19:54:35 +0200

    Sets and Union

---
+++ Sets_and_Union/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sets_and_Union
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[][] s = new string[n][];
+
+            for(int i = 0; i < n; i++)
+            {
+                string a = Console.ReadLine();
+                s[i] = a.Split(' ');
+            }
+
+
+
+            Console.WriteLine(s[1][1]);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
