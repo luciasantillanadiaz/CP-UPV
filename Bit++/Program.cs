5c67031819a2a3b8f943fc077746590c7d043a19
Lucía <luciasantillanadiaz@gmail.com>
2024-05-21 19:46:30 +0200

    Bit++

---
+++ Bit++/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit__
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int n = int.Parse(Console.ReadLine());
+            string a;
+            for(int i = 0; i < n; i++)
+            {
+                a = Console.ReadLine();
+                for(int j = 0; j < a.Length; j++)
+                {
+                    if (a.Substring(j,1) == "+")
+                    {
+                        x++;
+                        break;
+                    }
+                    if(a.Substring(j,1) == "-")
+                    {
+                        x--;
+                        break;
+                    }
+                }
+            }
+
+            Console.WriteLine(x);
+            Console.ReadKey();
+        }
+    }
+}
