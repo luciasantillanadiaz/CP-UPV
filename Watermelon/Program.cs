86059e24f476ac0c14620c522bb250f1d28cac61
Lucía <luciasantillanadiaz@gmail.com>
2024-05-21 19:56:46 +0200

    Watermelon

---
+++ Watermelon/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Watermelon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int w = 0;
+
+            
+            w = int.Parse(Console.ReadLine());
+
+            if (w % 2 == 0 && w > 2)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
+
+        }
+    }
+}
