824781b6f1a80f3592aefd07abdf2e098b543455
Lucía <luciasantillanadiaz@gmail.com>
2024-05-21 19:47:07 +0200

    Domino Piling

---
+++ Domino_Piling/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domino_Piling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = Console.ReadLine();
+            string[] b = a.Split(' ');
+            int m = int.Parse(b[0]);
+            int n = int.Parse(b[1]);
+            Console.WriteLine(m * n / 2);
+            Console.ReadKey();
+        }
+    }
+}
